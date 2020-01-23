4a8da7c6668c2978ca31d19b95b437019bb39ff1
matixezor <matixezor10@interia.pl>
2020-01-23 18:45:39 +0100

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+headers = {
+    'x-rapidapi-host': "api-football-v1.p.rapidapi.com",
+    'x-rapidapi-key': ""
+    }
+url = "https://api-football-v1.p.rapidapi.com/v2/fixtures/"
+my_team = ""
+ifttt_key = ""
+event_name = "game_end"
+ifttt_webhook_url = "https://maker.ifttt.com/trigger/" + event_name + "/with/key/" + ifttt_key

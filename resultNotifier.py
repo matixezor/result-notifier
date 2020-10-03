e301f7ce0d33d42589932c5b9deef8a4971b192f
matixezor <matixezor10@interia.pl>
2020-10-03 13:16:51 +0200

    add type hints, add goalscorers info to iftt webhook

--- resultNotifier.py
+++ resultNotifier.py
@@ -1,67 +1,79 @@
-import requests
 import time
+from typing import Dict, Any, Union
+
+import requests
+
 from config import headers, url, my_team, ifttt_webhook_url
 
 
-def connection_and_parse(url, headers):
+def connection_and_parse(url: str, headers: Dict[str, str]) -> Dict[str, Any]:
     response = requests.get(url, headers=headers)
     response = response.json()
-    
+
     return response["api"]["fixtures"][0]
 
 
-def join_strings(*args):
+def join_strings(*args: str) -> str:
     return " ".join(arg for arg in args)
 
-    
-def send_ifttt_notification(ifttt_webhook_url, match_result):
+
+def send_ifttt_notification(ifttt_webhook_url: str, match_result: Dict[str, str]):
     result_with_team_names = join_strings(
         match_result["home_team"],
         match_result["score"],
         match_result["away_team"]
-        )
+    )
     data = {
         "value1": match_result["league"],
-        "value2": result_with_team_names
-        }
-    
+        "value2": result_with_team_names,
+        "value3": match_result["goals"]
+    }
+
     requests.post(ifttt_webhook_url, json=data)
 
 
-def get_match_timestamp_and_id(url, headers, my_team):
+def get_match_timestamp_and_id(url: str, headers: Dict[str, str], my_team: str) -> Dict[str, Union[str, int]]:
     url += "team/" + my_team + "/next/1"
     response = connection_and_parse(url, headers)
-    
+
     return {
         "fixtureID": str(response["fixture_id"]),
         "timestamp": response["event_timestamp"]
-        }
+    }
 
 
-def wait_till_full_time(match_timestamp):
+def wait_till_full_time(match_timestamp: int):
     curr_timestamp = time.time()
     time_to_wait = match_timestamp - curr_timestamp + 7200
     time.sleep(time_to_wait)
-    
 
-def get_match_result(url, headers, fixture_id):
+
+def get_match_result(url: str, headers: Dict[str, str], fixture_id: str) -> Dict[str, str]:
     url += "id/" + fixture_id
     response = connection_and_parse(url, headers)
 
     while response["statusShort"] not in ["AEN", "PEN", "FT"]:
         # in case there's extra time or penalties sleep 15 min
         time.sleep(900)
-        response = connection_and_parse(url, headers)        
-    
+        response = connection_and_parse(url, headers)
+
+    goals = ''
+    if response["goalsHomeTeam"] + response["goalsAwayTeam"] != 0:
+        for event in response["events"]:
+            if event["type"] == "Goal":
+                time = event["elapsed"] + event["elapsed_plus"] if event["elapsed_plus"] else event["elapsed"]
+                goals += f"\n{time} - {event['player']}"
+
     return {
         "league": response["league"]["name"],
         "home_team": response["homeTeam"]["team_name"],
         "away_team": response["awayTeam"]["team_name"],
-        "score": str(response["goalsHomeTeam"]) + '-' + str(response["goalsAwayTeam"])
-        }
+        "score": str(response["goalsHomeTeam"]) + '-' + str(response["goalsAwayTeam"]),
+        "goals": goals
+    }
 
-    
-def main(headers, url, my_team, ifttt_webhook_url):
+
+def main(headers: Dict[str, str], url: str, my_team: str, ifttt_webhook_url: str):
     while True:
         match_timestamp_and_id = get_match_timestamp_and_id(url, headers, my_team)
         wait_till_full_time(match_timestamp_and_id["timestamp"])
@@ -70,5 +82,5 @@ def main(headers, url, my_team, ifttt_webhook_url):
         send_ifttt_notification(ifttt_webhook_url, match_result)
 
 
-if __name__== "__main__" :
+if __name__ == "__main__":
     main(headers, url, my_team, ifttt_webhook_url)
